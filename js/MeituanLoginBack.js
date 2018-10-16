ad1444bd3425d1c61be31c4c5b09e1d847a7a2d7
Baiguoshuai <1057459067@qq.com>
2018-10-16 22:26:49 +0800

    第11个版本，商品列表页面写了80%，脚本未写

--- js/MeituanLoginBack.js
+++ js/MeituanLoginBack.js
@@ -1,45 +1,38 @@
-//MeiTuanLogin1.html
-var localCookie=document.cookie;
-var cookieName=localCookie.split(";")[localCookie.split(";").length-1].split("=") [0];
-var cookieNameNum=localCookie.split(";")
-console.log(decodeURIComponent(cookieName))
-var strCookieN="";
-	
-	function deleteCookie(){
-		for(let i=0;i<cookieNameNum.length;i++){
-			strCookieN=cookieNameNum[i].split("=")[0];	//找出cookie的所有key
-	  		var date=new Date(); 
-	  		date.setTime(date.getTime()-10000); 
-	 		document.cookie=strCookieN+"=s; expires="+date.toGMTString()+";path="+"/";//如果给cookie设置了path，删除也要设置同样的格式
-	 		//document.cookie=strCookieN+"=s; expires="+date.toGMTString();
-		//$.removeCookie(strCookieN)	//不兼容IE
-			console.log(strCookieN)
+
+$(function(){
+
+	var localCookie=document.cookie;
+	var cookieName=localCookie.split(";")[localCookie.split(";").length-1].split("=") [0];
+	var cookieNameNum=localCookie.split(";")
+	console.log(decodeURIComponent(cookieName))
+	var strCookieN="";
+		
+		function deleteCookie(){
+			for(let i=0;i<cookieNameNum.length;i++){
+				strCookieN=cookieNameNum[i].split("=")[0];	//找出cookie的所有key
+		  		var date=new Date(); 
+		  		date.setTime(date.getTime()-10000); 
+		 		document.cookie=strCookieN+"=s; expires="+date.toGMTString()+";path="+"/";//如果给cookie设置了path，删除也要设置同样的格式
+		 		//document.cookie=strCookieN+"=s; expires="+date.toGMTString();
+			//$.removeCookie(strCookieN)	//不兼容IE
+				console.log(strCookieN)
+			}
+			location.href="MeiTuanIndex.html";
 		}
-		location.href="../MeiTuanIndex.html";
-	}
-	
-	$(".exit").click(function(){
-		deleteCookie();
-		//location.href="../MeiTuanIndex.html"
-	})
-	$(".username").html(function(){			//改变用户名
-		return decodeURIComponent(cookieName);
-	})
-	$(".username").click(function(){
-		console.log(decodeURIComponent(localCookie));
-	})
-	let slider=new Slider(				// 轮播图
-		$(".slider-imgBox")[0],
-		550,
-		240,
-		["../img/daa73310c9e57454dc97f0146640fd9f69772.jpg","../img/a97baf515235f4c5a2b1323a741e577185048.jpg","../img/33ff80dc00f832d697f3e20fc030799560495.jpg","../img/e419ae8488f776407459b0ee7eada070286451.jpg"],
-		10,
-		2,
-		"#eee",
-		"#fff",
-		false,
-		true,
-		"#",
-		3000
-	)
+		
+		$(".exit").click(function(){
+			deleteCookie();
+			//location.href="../MeiTuanIndex.html"
+		})
+		$(".username").html(function(){			//改变用户名
+			return decodeURIComponent(cookieName);
+		})
+		$(".username").prev().attr("src","../img/35ad1f9253761ea6ff822b5e659f234f3758.png")
+		
+		$(".username").click(function(){
+			console.log(decodeURIComponent(localCookie));
+		})
+		
+})
+
 
